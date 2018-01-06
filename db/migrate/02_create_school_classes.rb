59e2c7055bac5699b614ad881df3a0e484f56f10
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-01-06 21:46:42 +0000

    automatically backed up by learn

---
+++ db/migrate/02_create_school_classes.rb
@@ -0,0 +1,10 @@
+class CreateSchoolClasses < ActiveRecord::Migration
+  def change
+    create_table :school_classes do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps null: false
+    end
+  end
+end
